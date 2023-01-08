1c80ac91ad518380a4228d7eab197d933c0e19a9
Alex Pitzel <pitzelalex@gmail.com>
2023-01-08 13:17:14 -0500

    feat: dish show page display ingerdient names

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -11,7 +11,24 @@ RSpec.describe "dish/show.html.erb", type: :feature do
       expect(page).to have_content('Dish Description: description goes here')
     end
 
-    it 'displays a list of ingredients for that dish'
+    it 'displays a list of ingredients for that dish' do
+      chef = Chef.create!(name: 'Sir Alex Pitzel')
+      dish = chef.dishes.create!(name: 'Pizza', description: 'description goes here')
+      ingredient1 = Ingredient.create!(name: 'Pepperoni', calories: '300')
+      ingredient2 = Ingredient.create!(name: 'Mozzarella Cheese', calories: '400')
+      ingredient3 = Ingredient.create!(name: 'Pizza Dough', calories: '150')
+      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient1.id)
+      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient2.id)
+      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient3.id)
+      visit dish_path(dish)
+
+      within '#ingredients' do
+        expect(page).to have_content('Ingredients:')
+        expect(page).to have_content(ingredient1.name)
+        expect(page).to have_content(ingredient2.name)
+        expect(page).to have_content(ingredient3.name)
+      end
+    end
     it 'displays a total calorie count for that dish'
     it 'displays the chefs name'
   end
