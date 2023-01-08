80b349d7066c216e5720f964dbc0308b66186845
Alex Pitzel <pitzelalex@gmail.com>
2023-01-08 13:26:43 -0500

    feat: Create dish view display of total calories

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "dish/show.html.erb", type: :feature do
+
+  let!(:chef) { Chef.create!(name: 'Sir Alex Pitzel') }
+  let!(:dish) { chef.dishes.create!(name: 'Pizza', description: 'description goes here') }
+  let!(:ingredient1) { Ingredient.create!(name: 'Pepperoni', calories: '300') }
+  let!(:ingredient2) { Ingredient.create!(name: 'Mozzarella Cheese', calories: '400') }
+  let!(:ingredient3) { Ingredient.create!(name: 'Pizza Dough', calories: '150') }
+  let!(:di1) { DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient1.id) }
+  let!(:di2) { DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient2.id) }
+  let!(:di3) { DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient3.id) }
+
   describe 'when I visit a dishes show page' do
     it 'displays the dishes name and description' do
-      chef = Chef.create!(name: 'Sir Alex Pitzel')
-      dish = chef.dishes.create!(name: 'Pizza', description: 'description goes here')
       visit dish_path(dish)
 
       expect(page).to have_content('Dish Name: Pizza')
@@ -12,14 +20,7 @@ RSpec.describe "dish/show.html.erb", type: :feature do
     end
 
     it 'displays a list of ingredients for that dish' do
-      chef = Chef.create!(name: 'Sir Alex Pitzel')
-      dish = chef.dishes.create!(name: 'Pizza', description: 'description goes here')
-      ingredient1 = Ingredient.create!(name: 'Pepperoni', calories: '300')
-      ingredient2 = Ingredient.create!(name: 'Mozzarella Cheese', calories: '400')
-      ingredient3 = Ingredient.create!(name: 'Pizza Dough', calories: '150')
-      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient1.id)
-      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient2.id)
-      DishIngredient.create!(dish_id: dish.id, ingredient_id: ingredient3.id)
+
       visit dish_path(dish)
 
       within '#ingredients' do
@@ -29,7 +30,11 @@ RSpec.describe "dish/show.html.erb", type: :feature do
         expect(page).to have_content(ingredient3.name)
       end
     end
-    it 'displays a total calorie count for that dish'
+    it 'displays a total calorie count for that dish' do
+      visit dish_path(dish)
+
+      expect(page).to have_content('Total Calories: 850')
+    end
     it 'displays the chefs name'
   end
 end
